a5369866f9cd823a4d406047380c49b78255b5c8
Michael Jae-Yoon Chung <mjyc@cs.washington.edu>
2018-06-12 16:23:39 -0700

    Organize files

--- package.js
+++ package.js
@@ -1,24 +1,13 @@
 Package.describe({
   name: 'meteor-action',
   version: '0.0.1',
-  // Brief, one-line summary of the package.
-  summary: '',
-  // URL to the Git repository containing the source code for this package.
-  git: '',
-  // By default, Meteor will default to using README.md for documentation.
-  // To avoid submitting documentation, set this field to null.
+  summary: 'Meteor package for action, an interface for preemptable tasks',
+  git: 'https://github.com/mjyc/meteor-action',
   documentation: 'README.md'
 });
 
 Package.onUse(function(api) {
   api.versionsFrom('1.6');
   api.use('ecmascript');
-  api.mainModule('meteor-action.js');
-});
-
-Package.onTest(function(api) {
-  api.use('ecmascript');
-  api.use('tinytest');
-  api.use('meteor-action');
-  api.mainModule('meteor-action-tests.js');
+  api.mainModule('action.js');
 });
